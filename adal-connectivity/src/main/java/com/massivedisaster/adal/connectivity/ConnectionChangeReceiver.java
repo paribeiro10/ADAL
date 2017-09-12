4a5fbd103fc4c7226f1a236b811192496b17aa72
Paulo <paulo.ribeiro@simdea.pt>
2017-09-12 21:41:11 +0100

    Correction of a Checkstyle rule violation

--- adal-connectivity/src/main/java/com/massivedisaster/adal/connectivity/ConnectionChangeReceiver.java
+++ adal-connectivity/src/main/java/com/massivedisaster/adal/connectivity/ConnectionChangeReceiver.java
@@ -25,7 +25,6 @@
 
 package com.massivedisaster.adal.connectivity;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
